6c8127e8d2acc213490e2cc87dfe6cd0908f4788
Matheus Gundler <matheusgrundler@gmail.com>
2019-06-28 11:40:11 -0300

    lógica pronto falta montar o módulo e rafatorar o código

--- index.js
+++ index.js
@@ -1,89 +1,60 @@
 var express = require("express"),
-    fs = require("fs"),
-    request = require("request"),
-    cheerio = require("cheerio"),
-    app = express();
+  fs = require("fs"),
+  request = require("request"),
+  cheerio = require("cheerio"),
+  app = express();
 
 // Passo 1
-app.get("/raspagem", function(req, res) {
-    url = "https://www.americanas.com.br/produto/133718358";
+app.get("/", function(req, res) {
+  url = "https://www.americanas.com.br/produto/133756522";
 
-    request(url, function(error, response, html) {
-        if (!error) {
-            var $ = cheerio.load(html);
+  request(url, function(error, response, html) {
+    if (!error) {
+      var $ = cheerio.load(html);
 
-            // Objeto que irá armazenar a tabela
-            var resultado = [];
+      var id = $(".brNcBx")
+        .text()
+        .trim();
 
-            var id = $(".brNcBx")
-                .text()
-                .trim();
+      var breadcrumbs = [];
 
-            var breadcrumbs = $(".BreadcrumbContainer-iw976r-0").each(function(i) {
-                console.log(
-                    $(this)
-                    .find("span")
-                    .text()
-                    .trim()
-                );
-            });
+      var name = $(".fPUiKP")
+        .find("h1")
+        .text();
 
-            var title = $(".fPUiKP")
-                .find("h1")
-                .text();
-            var imagen = $(".gallery")
-                .find("img")
-                .attr("src");
+      var img = $(".gallery")
+        .find("img")
+        .attr("src");
 
-            var seller = $("seller-00776574000660").text();
-            var price = $(".sales-price").text();
-            console.log(price);
-            process.exit();
+      var seller = $(".seller-name-container span.TextUI-sc-1hrwx40-0").text();
 
-            // Passo 3
-            // Manipulando o seletor específico para montar nossa estrutura
-            // Escolhi não selecionar a primeira linha porque faz parte do header da tabela
-            // $(".ViewSection-oocyw8-2").each(function(i) {
-            //     // Obtendo as propriedades da tabela.
-            //     // O método .trim() garante que irá remover espaço em branco
+      var price = $(
+        ".main-offer__ParagraphUI-sc-1oo1w8r-0 .sales-price"
+      ).text();
 
-            //     console.log($(this).find("span"));
-            //     process.exit();
+      id = parseInt(id.replace(/[{(Cód.)}]/g, ""));
 
-            //     var codigo = $(this)
-            //         .find("td")
-            //         .eq(0)
-            //         .text()
-            //         .trim(),
-            //         orgao = $(this)
-            //         .find("td")
-            //         .eq(1)
-            //         .text()
-            //         .trim(),
-            //         valorTotal = $(this)
-            //         .find("td")
-            //         .eq(2)
-            //         .text()
-            //         .trim();
+      $(".BreadcrumbContainer-iw976r-0 span").each(function(i) {
+        breadcrumbs.push($(this).text());
+      });
 
-            //     // Inserindo os dados obtidos no nosso objeto
-            //     resultado.push({
-            //         codigo: codigo,
-            //         orgao: orgao,
-            //         total: valorTotal
-            //     });
-            // });
-            fs.writeFile(
-                "resultado.json",
-                JSON.stringify(resultado, null, 4),
-                function(err) {
-                    console.log(
-                        "JSON escrito com sucesso! O arquivo está na raiz do projeto."
-                    );
-                }
-            );
-        }
-    });
+
+      price = price.replace("R$ ", "");
+      price = price.replace(".", "");
+      price = parseFloat(price.replace(",", "."));
+
+      var resultado = {
+        id: id,
+        breadcrumbs: breadcrumbs,
+        name: name,
+        img: img,
+        seller: seller,
+        price: price
+      };
+
+      console.log(JSON.stringify(resultado));
+    }
+  });
 });
 
 app.listen("8081");
